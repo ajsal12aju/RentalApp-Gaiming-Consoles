86efeae8822d0a6037fd01ef18a0bc119b3be920
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-02-20 21:56:41 +0530

    updated

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { saveToWishlist, addToCart } from "../contriollers/user.js";
+import { saveToWishlist, addToCart, placeOrder } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.put("/wishlist", saveToWishlist);
 router.put("/cart", addToCart);
+router.post("/order", placeOrder); 
 
 export default router;
